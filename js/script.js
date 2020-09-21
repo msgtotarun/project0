1f89c17580af2e87b64c00e381dc22437bda2444
tarun <msgtotarun@gmail.com>
2020-09-21 13:22:03 +0530

    master

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const toggleBtn = document.getElementsByClassName('toggle-btn')[0]
+const navLinks = document.getElementsByClassName('nav-links')[0]
+toggleBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active')
+})
+
+function goto() {
+   
+}
